39a6b146f43e7d6a62edc98f1cb35dc1f1a554e6
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-05-26 00:33:28 +0530

    Add files via upload

---
+++ Session1Assignment1.java
@@ -0,0 +1,28 @@
+
+public class Session1Assignment1 {
+
+	public static void main(String[] args) {
+		
+		
+		//Write a program to print the sum of two numbers. One number should be int and another should be float.
+		
+		int a = 20;
+		float b = 40;
+		float sum = a+b;
+		
+		System.out.println("The sum of two numbers are " + sum);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
